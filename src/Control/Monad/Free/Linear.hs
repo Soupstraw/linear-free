a19f349743ec767505f379750e18d7ef8275ce35
Joosep Jääger <joosep.jaager@gmail.com>
2025-07-17 22:59:34 +0300

    Added an example to tests

--- src/Control/Monad/Free/Linear.hs
+++ src/Control/Monad/Free/Linear.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE LambdaCase #-}
 
 module Control.Monad.Free.Linear (
   MonadFreeL (..),
@@ -15,15 +15,15 @@ module Control.Monad.Free.Linear (
   hoistFree,
   foldFree,
   unfold,
+  liftF,
 ) where
 
 import qualified Control.Functor.Linear as Control
-import Data.Functor (Functor)
 import qualified Data.Functor.Linear as Data
 import GHC.Generics (Generic)
 import Prelude.Linear
 
-class MonadFreeL f m where
+class Control.Monad m => MonadFreeL f m where
   wrap :: f (m a) %1 -> m a
 
 data FreeL f a where
@@ -31,37 +31,35 @@ data FreeL f a where
   Free :: f (FreeL f a) %1 -> FreeL f a
   deriving (Generic)
 
-instance (Functor f, Data.Functor f) => Data.Functor (FreeL f) where
+instance Data.Functor f => Data.Functor (FreeL f) where
   fmap f (Pure x) = Pure $ f x
   fmap f (Free m) = Free $ Data.fmap (Data.fmap f) m
 
-instance (Functor f, Control.Functor f) => Control.Functor (FreeL f) where
+instance Control.Functor f => Control.Functor (FreeL f) where
   fmap f (Pure x) = Pure $ f x
   fmap f (Free m) = Free $ Control.fmap (Control.fmap f) m
 
-instance (Functor f, Control.Functor f, Data.Applicative f) => Data.Applicative (FreeL f) where
+instance Control.Functor f => Data.Applicative (FreeL f) where
   {-# INLINE pure #-}
   pure = Pure
 
-  Pure f <*> Pure x = Pure $ f x
-  Pure f <*> Free mx = Free $ Control.fmap (Control.fmap f) mx
-  Free mf <*> Pure x = Free $ Control.fmap ($ x) Control.<$> mf
-  Free ma <*> Free mb = Free $ Control.fmap (Data.<*>) ma Data.<*> mb
+  Pure a <*> Pure b = Pure $ a b
+  Pure a <*> Free mb = Free $ Control.fmap a Control.<$> mb
+  Free ma <*> b = Free $ (Control.<*> b) Control.<$> ma
 
-instance (Functor f, Control.Applicative f) => Control.Applicative (FreeL f) where
+instance Control.Functor f => Control.Applicative (FreeL f) where
   {-# INLINE pure #-}
   pure = Pure
 
-  Pure f <*> Pure x = Pure $ f x
-  Pure f <*> Free mx = Free $ Control.fmap (Control.fmap f) mx
-  Free mf <*> Pure x = Free $ Control.fmap ($ x) Control.<$> mf
-  Free ma <*> Free mb = Free $ Control.fmap (Control.<*>) ma Control.<*> mb
+  Pure a <*> Pure b = Pure $ a b
+  Pure a <*> Free mb = Free $ Control.fmap a Control.<$> mb
+  Free ma <*> b = Free $ (Control.<*> b) Control.<$> ma
 
-instance (Functor f, Control.Applicative f) => Control.Monad (FreeL f) where
+instance Control.Functor f => Control.Monad (FreeL f) where
   Pure x >>= f = f x
   Free m >>= f = Free ((Control.>>= f) Control.<$> m)
 
-instance MonadFreeL f (FreeL f) where
+instance Control.Functor f => MonadFreeL f (FreeL f) where
   {-# INLINE wrap #-}
   wrap = Free
 
@@ -86,3 +84,6 @@ unfold :: Control.Functor f => (b %1 -> Either a (f b)) -> b %1 -> FreeL f a
 unfold f b = case f b of
   Left x -> Pure x
   Right m -> Free $ unfold f Control.<$> m
+
+liftF :: (Control.Functor f, MonadFreeL f m) => f a %1 -> m a
+liftF = wrap . Control.fmap Control.pure
--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,83 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE QualifiedDo #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
-import Test.Hspec (describe, hspec)
-import Test.Hspec.QuickCheck (prop)
+import qualified Control.Functor.Linear as Control
+import qualified Control.Functor.Linear as Linear
+import Control.Monad.Free.Linear (FreeL (..), liftF)
+import qualified Data.Functor.Linear as Data
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Prelude.Linear hiding ((.))
+import qualified Prelude.Linear as L
+import Test.Hspec (describe, hspec, it, shouldBe)
+import qualified Prelude as Base
+
+data ProductType
+  = Eggs
+  | Milk
+  | Flour
+  | Dough
+  | Pancake
+  deriving (Base.Eq, Base.Ord, Show)
+
+newtype ProductId = ProductId Int
+
+data RecipeF a where
+  Buy :: ProductType -> (ProductId %1 -> a) %1 -> RecipeF a
+  Combine :: ProductType -> [ProductId] %1 -> (ProductId %1 -> a) %1 -> RecipeF a
+  Cook :: ProductType -> ProductId %1 -> (ProductId %1 -> a) %1 -> RecipeF a
+
+instance Data.Functor RecipeF where
+  fmap f (Buy t c) = Buy t $ f L.. c
+  fmap f (Combine t cs c) = Combine t cs $ f L.. c
+  fmap f (Cook t p c) = Cook t p $ f L.. c
+
+instance Control.Functor RecipeF where
+  fmap f (Buy t c) = Buy t $ f L.. c
+  fmap f (Combine t cs c) = Combine t cs $ f L.. c
+  fmap f (Cook t p c) = Cook t p $ f L.. c
+
+type Recipe a = FreeL RecipeF a
+
+buy :: ProductType -> Recipe ProductId
+buy t = liftF $ Buy t id
+
+combine :: ProductType -> [ProductId] %1 -> Recipe ProductId
+combine t ps = liftF $ Combine t ps id
+
+cook :: ProductType -> ProductId %1 -> Recipe ProductId
+cook t p = liftF $ Cook t p id
+
+pancakeRecipe :: Recipe ProductId
+pancakeRecipe = Linear.do
+  eggs <- buy Eggs
+  milk <- buy Milk
+  flour <- buy Flour
+  dough <- combine Dough [eggs, milk, flour]
+  pancake <- cook Pancake dough
+  Linear.pure pancake
+
+gatherIngredients :: Recipe a -> Map ProductType Int
+gatherIngredients (Pure _) = Map.empty
+gatherIngredients (Free x) = case x of
+  Buy t c -> Map.insertWith (Base.+) t 1 $ gatherIngredients (c productId)
+  Combine _ _ c -> gatherIngredients $ c productId
+  Cook _ _ c -> gatherIngredients $ c productId
+  where
+    -- Doesn't matter what we put here, it will not be used anyways
+    productId = ProductId 0
 
 main :: IO ()
-main = hspec . describe "Linear free monad" $ do
-  undefined
+main = hspec $ describe "Linear free monad" $ do
+  describe "pancake recipe example" $ do
+    it "can gather the ingredients" $ do
+      gatherIngredients pancakeRecipe `shouldBe` Map.fromList
+        [ (Eggs, 1)
+        , (Milk, 1)
+        , (Flour, 1)
+        ]
